cddb38deb2642f0ce5ac9fa520ad3ff76e122dbc
spwpun <32606457+spwpun@users.noreply.github.com>
2023-02-21 15:03:24 +0800

    Update pwn_init_py3.sh

--- pwn_init_py3.sh
+++ pwn_init_py3.sh
@@ -12,9 +12,9 @@ cd ~/
 echo "I suggest you modify the /etc/apt/sources.list file to speed up the download."
 echo "Press Enter to continue~"
 read -t 5 test
-#sudo  sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+#  sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 # change sourse —— deb-src 
-sudo sed -i 's/# deb-src/deb-src/' "/etc/apt/sources.list"
+ sed -i 's/# deb-src/deb-src/' "/etc/apt/sources.list"
 # change pip source
 if [ ! -d ~/.pip ]; then
   mkdir ~/.pip
@@ -22,11 +22,15 @@ fi
 echo -e "[global]\nindex-url = https://pypi.douban.com/simple/\n[install]\ntrusted-host = pypi.douban.com" >  ~/.pip/pip.conf
 # support 32 bit
 dpkg --add-architecture i386
-sudo apt-get update
-# sudo apt-get -y install lib32z1
-sudo apt-get -y install libc6-i386
+ apt-get update
+#  apt-get -y install lib32z1
+ apt-get -y install libc6-i386
 # maybe git？
-sudo apt-get -y install git gdb
+ apt-get -y install git gdb
+# install pwntools
+ apt-get -y install python3 python3-pip
+ pip3 install 'cryptography==38.0.4'
+ pip3 install pwntools
 # install pwndbg
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
@@ -35,13 +39,10 @@ cd pwndbg
 git clone https://github.com/longld/peda.git ~/peda
 echo "source ~/peda/peda.py" >> ~/.gdbinit
 # download the libc source to current directory(you can use gdb with this example command: directory ~/glibc-2.24/malloc/)
-sudo apt-get source libc6-dev
-# install pwntools
-sudo apt-get -y install python3 python3-pip
-pip3 install pwntools
+ apt-get source libc6-dev
 # install one_gadget
-sudo apt-get -y install ruby
-sudo gem install one_gadget
+ apt-get -y install ruby
+ gem install one_gadget
 # download 
 git clone https://github.com/niklasb/libc-database.git ~/libc-database
 echo "Do you want to download libc-database now(Y/n)?"
